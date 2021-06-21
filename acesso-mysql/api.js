c366ab3dee8d453e28802b2d137e5ab71a4b8622
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-06-21 20:52:58 -0300

    add connection pool

--- acesso-mysql/api.js
+++ acesso-mysql/api.js
@@ -11,15 +11,19 @@ const run = async() => {
     //console.log(list)
 
     //await products.create(['Ibanez RG550', 123])
-    await products.updateCategories(5, [1, 3, 5])
+    //await products.updateCategories(5, [1, 3, 5])
 
     //await products.addImage(5, ['texto img RG550', 'about:blank'])
     //await products.remove(3)
 
     //const list = await products.findAll()
     //const list = await products.findAllByCategory(1)
-    const list = await products.findAllPaginated({pageSize: 2, currentPage: 1})
-    console.log(list)
+    //const list = await products.findAllPaginated({pageSize: 2, currentPage: 1})
+    //console.log(list)
+
+    //for (let i = 0; i< 1000; i++) {
+        //products.findAllPaginated({pageSize: 2, currentPage: 1}).then(list => console.log(list))
+    //}
 
 }
 
